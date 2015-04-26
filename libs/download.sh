a8dde19240458580f04b88ccb2d357c84a6b156a
metal-oxide-semiconductor <bbarantschik@gmail.com>
2015-04-26 02:53:24 -0700

    Fixed download.sh problems

--- libs/download.sh
+++ libs/download.sh
@@ -13,5 +13,23 @@ rm -rf assimp
 git clone https://github.com/nigels-com/glew.git glew
 git clone https://github.com/glfw/glfw.git glfw
 git clone https://github.com/epatel/SOIL.git SOIL
-git clone https://github.com/g-truc/glm.git glm
-git clone https://github.com/assimp/assimp.git assimp
+
+wget 'http://downloads.sourceforge.net/project/ogl-math/glm-0.9.6.3/glm-0.9.6.3.zip' -O glm.zip
+unzip glm.zip -d glm_tmp
+rm glm.zip
+mv glm_tmp/glm glm
+rm -rf glm_tmp
+
+wget 'http://downloads.sourceforge.net/project/assimp/assimp-3.1/assimp-3.1.1_no_test_models.zip' -O assimp.zip
+unzip assimp.zip -d assimp_tmp
+rm assimp.zip
+mv assimp_tmp/* assimp
+rm -rf assimp_tmp
+
+# Make glew
+cd glew/auto
+make
+
+#Move SOIL to make namespace clearer
+cd ..
+mv SOIL/src/ SOIL/SOIL
