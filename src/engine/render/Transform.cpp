3e81936a758a9092d513065939e4637bd20d076a
metal-oxide-semiconductor <bbarantschik@gmail.com>
2015-07-12 17:37:08 -0700

    created simple transform api

--- src/engine/render/Transform.cpp
+++ src/engine/render/Transform.cpp
@@ -1,1 +1,32 @@
 #include "Transform.hpp"
+
+Transform *Transform::rotate(double theta)
+{
+
+	return this;
+}
+
+Transform *Transform::scale(double scaleX, double scaleY)
+{
+	return this;
+}
+
+Transform *Transform::translate(double dx, double dy)
+{
+	return this;
+}
+
+Transform *Transform::multiply(Transform other)
+{
+	return this;
+}
+
+void Transform::attach(Transform parent)
+{
+
+}
+
+Transform Transform::multiply(Transform a, Transform b)
+{
+	return Transform();
+}
--- src/engine/render/Transform.hpp
+++ src/engine/render/Transform.hpp
@@ -1,6 +1,19 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 class Transform
 {
+private:
+	glm::mat4 base;
+	glm::mat4 *parent;
 
+public:
+	Transform *rotate(double theta);
+	Transform *scale(double scaleX, double scaleY);
+	Transform *translate(double dx, double dy);
+	Transform *multiply(Transform other); //multiplies internally and returns self
+	void attach(Transform parent); //always follows another transform
+
+	static Transform multiply(Transform a, Transform b); //multiplies externally
 };
