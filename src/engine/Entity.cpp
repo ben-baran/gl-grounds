989d29f99fcbaaf5d3e595965c6429100112f329
metal-oxide-semiconductor <bbarantschik@gmail.com>
2015-07-13 15:48:59 -0700

    Created entity constructors, created Collider and Renderable classes

--- src/engine/Entity.cpp
+++ src/engine/Entity.cpp
@@ -1,1 +1,11 @@
 #include "Entity.hpp"
+
+Entity::Entity(Renderable *renderable, Transform *transform)
+{
+
+}
+
+Entity::Entity(Renderable *renderable, Collider *collider, Transform *transform)
+{
+
+}
--- src/engine/Entity.hpp
+++ src/engine/Entity.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <src/engine/render/Transform.hpp>
+#include <src/engine/render/Renderable.hpp>
+#include <src/engine/physics/Collider.hpp>
+
 class Entity
 {
-
+public:
+	Entity(Renderable *renderable, Transform *transform = new Transform());
+	Entity(Renderable *renderable, Collider *collider, Transform *transform = new Transform());
 };
