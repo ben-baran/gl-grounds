ef88850fee68a42d6353c4b5726060b277e26de6
metal-oxide-semiconductor <bbarantschik@gmail.com>
2015-07-13 19:42:44 -0700

    created entity importance

--- src/engine/Entity.cpp
+++ src/engine/Entity.cpp
@@ -1,8 +1,42 @@
 #include "Entity.hpp"
+#include "Scene.hpp"
+
+using std::string
+
+double Entity::importance = 0;
 
 Renderable *Entity::renderable;
 Collider *Entity::collider;
 Transform *Entity::transform;
+
+double Entity::getImportance()
+{
+	return importance;
+}
+
+void Entity::setImportance(double importance)
+{
+	this->importance = importance;
+}
+
+void Entity::setImportanceBefore(std::initializer_list<string> names)
+{
+	double min = INFINITY;
+	for(string name : names) min = std::min(min, Scene::get(name).getImportance());
+	importance = min - 1;
+}
+
+void Entity::setImportanceBetween(string before, string after)
+{
+	importance = (Scene::get(before).getImportance() + Scene::get(after).getImportance()) / 2;
+}
+
+void Entity::setImportanceAfter(std::initializer_list<string> names)
+{
+	double max = -INFINITY;
+	for(string name : names) max = std::max(max, Scene::get(name).getImportance());
+	importance = max + 1;
+}
 
 Renderable &Entity::getRenderable()
 {
--- src/engine/Entity.hpp
+++ src/engine/Entity.hpp
@@ -4,13 +4,23 @@
 #include <src/engine/render/Renderable.hpp>
 #include <src/engine/physics/Collider.hpp>
 
+#include <string>
+
 class Entity
 {
 private:
+	double importance;
+
 	Renderable *renderable;
 	Collider *collider;
 	Transform *transform;
 public:
+	double getImportance();
+	void setImportance(double importance);
+	void setImportanceBefore(std::initializer_list<std::string> names);
+	void setImportanceBetween(std::string before, std::string after);
+	void setImportanceAfter(std::initializer_list<std::string> names);
+
 	Renderable &getRenderable();
 	Collider &getCollider();
 	Transform &getTransform();
