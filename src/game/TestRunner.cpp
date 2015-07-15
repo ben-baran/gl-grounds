696a80e43408e231bfa0a6c06d269e5a7d457113
ben-baran <bbarantschik@gmail.com>
2015-07-15 00:40:58 -0700

    finished implementing main in TestRunner

--- src/game/TestRunner.cpp
+++ src/game/TestRunner.cpp
@@ -1,6 +1,8 @@
 #include <src/engine/Entity.hpp>
 #include <src/engine/render/SolidRectangle.hpp>
 #include <src/engine/Scene.hpp>
+#include <src/engine/util/KeyHandler.hpp>
+#include <src/engine/util/Window.hpp>
 #include "TestRunner.hpp"
 
 int main()
@@ -15,12 +17,21 @@ void TestRunner::setup()
 
 	Entity *secondary = new Entity(new SolidRectangle(-0.1, -0.1, 0.2, 0.2, 0.9, 0.5, 0.5));
 	Scene::add("secondary", *secondary);
-	secondary->getRenderable().setLayerAbove({"player"});
+	secondary->getRenderable().setLayerBelow({"player"});
+	secondary->getTransform().translate(0.5, 0).attach(&(player->getTransform()));
 }
 
 void TestRunner::update(double dt)
 {
+	if(KeyHandler::pressed(GLFW_KEY_ESCAPE)) end();
+	Scene::get("player").getTransform().rotate(0.01);
+	Scene::get("secondary").getTransform().rotate(-0.01);
 
+	Transform &transform = Scene::get("player").getTransform();
+	if(KeyHandler::held(GLFW_KEY_W)) transform.translate(0, 0.04);
+	if(KeyHandler::held(GLFW_KEY_A)) transform.translate(-0.04, 0);
+	if(KeyHandler::held(GLFW_KEY_S)) transform.translate(0, -0.04);
+	if(KeyHandler::held(GLFW_KEY_D)) transform.translate(0.04, 0);
 }
 
 void TestRunner::cleanup(){}
