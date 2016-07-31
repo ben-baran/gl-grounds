c35d1528dd235096d7bfa938cf32c772454dd3af
Ben B <bbarantschik@gmail.com>
2016-07-30 20:32:19 -0800

    Revert "Revert "overhaul begins now""
    This reverts commit 8e6ecdc112275fbe50ef72ffe90d6dc98c8d85d6.

--- src/engine/Entity.hpp
+++ src/engine/Entity.hpp
@@ -14,12 +14,7 @@ private:
 	double importance;
 
 	std::unordered_set<std::string> tags;
-	std::unordered_map<std::string, bool> bProperties;
-	std::unordered_map<std::string, int> iProperties;
-	std::unordered_map<std::string, float> fProperties;
-	std::unordered_map<std::string, double> dProperties;
-	std::unordered_map<std::string, std::string> sProperties;
-	std::unordered_map<std::string, Entity*> eProperties;
+	std::unordered_map<std::string, void *> properties;
 
 	Renderable *renderable;
 	Collider *collider;
@@ -40,12 +35,17 @@ public:
 	void addTag(std::string tag);
 	void removeTag(std::string tag);
 
-	bool &bProperty(std::string name);
-	int &iProperty(std::string name);
-	float &fProperty(std::string name);
-	double &dProperty(std::string name);
-	std::string &sProperty(std::string name);
-	Entity *&eProperty(std::string name);
+    template <class T> T& get(std::string name) {
+        return *((T*) properties[name]);
+    }
+
+    template <class T> void set(std::string name, T t) {
+        properties[name] = &t;
+    }
+
+    template <class T> void setPointer(std::string name, T *t) {
+        properties[name] = t;
+    }
 
 	Entity(Renderable *renderable, Transform *transform = new Transform());
 	Entity(Renderable *renderable, Collider *collider, Transform *transform = new Transform());
